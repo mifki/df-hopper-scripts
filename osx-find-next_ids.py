9a0101f0bdceba232debd57426417516be0195bc
Vitaly Pronkin <pronvit@me.com>
2016-07-09 11:55:42 +1200

    first scripts

---
+++ osx-find-next_ids.py
@@ -0,0 +1,63 @@
+doc = Document.getCurrentDocument()
+seg = doc.getSegmentByName('__TEXT')
+
+def readAscii(seg,addr,end):
+	start = seg
+	ret = ''
+	started = 0
+	while addr < end:
+		b = seg.readByte(addr)
+		addr = addr + 1
+		if not b:
+			if started:
+				return ret, started
+			else:
+				continue
+		ret = ret + chr(b)
+		if not started:
+			started = addr-1
+	
+
+def process(seg, s, a):	
+	addr = seg.getReferencesOfAddress(a)[0]
+	while addr > seg.getStartingAddress():
+		addr = seg.instructionStart(addr-1)
+		i = seg.getInstructionAtAddress(addr)
+		if i.getInstructionString() == 'jl':
+			break
+
+	addr = seg.instructionStart(addr-1)
+	i = seg.getInstructionAtAddress(addr)
+	if i.getInstructionString() != 'cmp':
+		print 'can not find cmp instruction for %s' % s
+		return
+
+	reg = i.getFormattedArgument(1)[1:4]
+	while addr > seg.getStartingAddress():
+		addr = seg.instructionStart(addr-1)
+		i = seg.getInstructionAtAddress(addr)
+		if i.getInstructionString() == 'lea':
+			if i.getFormattedArgument(0) == reg:
+				gaddr = i.getFormattedArgument(1)[1:-1]
+				gaddr = int(gaddr, 16)
+				print "<global-address name='%s_next_id' value='0x%016x'/>" % (s, gaddr)
+				break
+
+
+print '============================='
+
+sec = doc.getSectionByName('__cstring')
+addr = sec.getStartingAddress()
+last = addr + sec.getLength()
+
+while addr < last:
+	s,ad = readAscii(seg,addr,last)
+
+	if s.find('Load Game: Invalid') != -1:
+		j = s.find('ID Number')
+		s = s[19:j-1].lower().replace(' ', '_')
+		process(seg,s,ad)
+
+	addr = ad+len(s)+1
+
+
---
+++ osx-find-vtable.py
@@ -0,0 +1,76 @@
+import re
+
+doc = Document.getCurrentDocument()
+dataseg = doc.getSegmentByName('__DATA')
+textseg = doc.getSegmentByName('__TEXT')
+
+def readAscii(seg,addr,end):
+	start = seg
+	ret = ''
+	started = 0
+	while addr < end:
+		b = seg.readByte(addr)
+		addr = addr + 1
+		if not b:
+			if started:
+				return ret, started
+			else:
+				continue
+		ret = ret + chr(b)
+		if not started:
+			started = addr-1
+
+def process(classname,addr,end):
+	refs = textseg.getReferencesOfAddress(addr)
+	if len(refs) == 0:
+		print 'can not find refs for', classname
+		return
+	a = refs[0]
+	if a < textseg.getStartingAddress():
+		a = refs[1]
+	a = a - 8
+
+	refs = dataseg.getReferencesOfAddress(a)
+	if len(refs) == 0:
+		print 'can not find more refs for', classname
+		return
+
+	vtable = refs[0] + 8
+	print "<vtable-address name='%s' value='0x%016x'/>" % (classname, vtable)
+	
+
+#######################################################
+for s in dataseg.getSectionsList():
+	if s.getName() == '__const':
+		sec = s
+		break
+
+i = sec.getStartingAddress()
+end = i + sec.getLength()
+
+while i < end:
+	dataseg.setTypeAtAddress(i, 8, Segment.TYPE_LONG)
+	i = i + 8
+######################################################
+
+print '============================='
+
+######################################################
+for s in textseg.getSectionsList():
+	if s.getName() == '__const':
+		sec = s
+		break
+
+addr = sec.getStartingAddress()
+end = addr + sec.getLength()
+
+ss = -1
+while addr < end:
+	s,ad = readAscii(textseg,addr,end)
+	textseg.setTypeAtAddress(ad, len(s)+1, Segment.TYPE_ASCII)
+	if re.match("[0-9]+[a-z_]+.+st", s):
+		classname = s[2:]
+		process(classname, ad, end)
+	addr = ad+len(s)+1
+#######################################################
+			
