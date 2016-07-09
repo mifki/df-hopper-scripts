504e96ebbce703528ed9e557ec3364cdc9f99388
Vitaly Pronkin <pronvit@me.com>
2016-07-09 23:23:58 +1200

    preliminary support for finding compound globals

---
+++ osx-find-compound-globals.py
@@ -0,0 +1,40 @@
+atexit = 'imp___symbol_stub1____cxa_atexit'
+
+doc = Document.getCurrentDocument()
+seg = doc.getSegmentByName('__TEXT')
+
+atexitaddr = doc.getAddressForName(atexit)
+
+refs = seg.getReferencesOfAddress(atexitaddr)
+
+globals = []
+gprocs = {}
+
+for addr in refs:
+	i = seg.getInstructionAtAddress(addr)
+	if i.getInstructionString() == 'call':	
+		addr2 = addr
+		while addr2 > seg.getStartingAddress():
+			addr2 = seg.instructionStart(addr2-1)
+			i = seg.getInstructionAtAddress(addr2)
+			if i.getInstructionString() == 'lea' and i.getFormattedArgument(0) == 'rsi':
+				gaddr = i.getFormattedArgument(1)[1:-1]
+				gaddr = int(gaddr, 16)
+				globals.append(gaddr)
+				gprocs[gaddr] = addr #doc.getNameAtAddress(p.getEntryPoint())
+				break
+
+globals.sort()
+
+print '============================='
+
+for i in range(0,len(globals)-1):
+	offset = globals[i]
+	size = globals[i+1]-globals[i]
+	callsite = gprocs[offset]
+	p = seg.getProcedureAtAddress(callsite)
+	ctorlist = 'sub_'+("%x" % p.getEntryPoint())
+
+	print '<global-object name="?" offset="0x%x" size="%d">' % (offset, size)
+	print '    <comment>ctorlist="%s" callsite="0x%x" </comment>' % (ctorlist, callsite)
+	print '</global-object>'
